c57158c5dd648ae8216ac207339de263ed5c793d
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-09 23:47:34 +0600

    last commit

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
-const searchFood = () => {
-    const foodText = document.getElementById('foodMeal').value;
-    console.log(foodText);
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s${foodText}`
+const searchFood = () =>{
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s`
     fetch(url)
-        .then(res => res.json())
-        .then(data => displayFood(data.meals))
-        .catch(err => displayError('Something Went Wrong please try later!'))
-        
+    .then(response => response.json())
+    .then(data => displayFood(data.meals))
 }
 
+const displayFood = foods =>{
+    const foodContainer = document.getElementById('food-container');
 
-const displayFood = names => {
-    const recipes = document.getElementById('recipes');
-    recipes.innerHTML = '';
-    names.forEach(name => {
-        const recipesDiv = document.createElement('div');
-        recipesDiv.className = 'search-result container2';
-        recipesDiv.innerHTML = `
-        <h3 class="mealDetails">${name.strMeal}</h3
-        <div class="item">
-            <img src="${name.strMealThumb}" alt="images">
+    foods.forEach(food => {
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'single-result row align-items-center my-3 p-3';
+        foodDiv.innerHTML = `
+         <div class="col-md-9">
+            <h3 class="food-name">${food.strMeal}</h3>
+            <p class="">Made by <span>${food.strArea}</span></p>
         </div>
-        <div class="container">
-                <h1 class="title"> ${name.strMeal}</h1>
+        <div class="images">
+            <img onclick="getDetails('${food.info}','${food.info}')" src="${food.strMealThumb}">
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+
         </div>
         `;
-        recipes.appendChild(recipesDiv);
+        foodContainer.appendChild(foodDiv);
+
     })
-
 }
 
-const recipeDetails = (mealName) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s${mealName}`
+const getDetails = (meal, recipe) =>{
+    const url = `http://https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}/ ${recipe}`;
     fetch(url)
-        .then(res => res.json())
-        .then(data => displayItemDetails(data.meals))
+    .then(response => response.json())
+    .then(data => displayItem(data.meals));
 }
 
-const displayItemDetails = info => {
-    const details = document.getElementById('recipe-info');
-    details.innerHTML = `
-        <img src="${info[0].strMealThumb}" alt="">
-        <h1 class="title">${info[0].strMeal}</h1>
+const displayItem = recipe =>{
+    const recipeInfo = document.getElementById('recipe-info');
+
+    recipe.forEach(info =>{
+        const recipeDiv = document.createElement('div');
+        recipeDiv.innerText = `
+        <img src="${info.idMeal}" alt="">
+        <h1 class="title">${info.strArea}</h1>
         <p>Ingredients</p>
         <ul>
-            <li>${info[0].strIngredient1}<li>
-            <li>${info[0].strIngredient2}<li>
-            <li>${info[0].strIngredient3}<li>
-            <li>${info[0].strIngredient4}<li>
-            <li>${info[0].strIngredient5}<li>
-            <li>${info[0].strIngredient6}<li>
-            <li>${info[0].strIngredient7}<li>
-            <li>${info[0].strIngredient8}<li>
+            <li>${info.strIngredient1}<li>
+            <li>${info.strIngredient2}<li>
+            <li>${info.strIngredient3}<li>
+            <li>${info.strIngredient4}<li>
+            <li>${info.strIngredient5}<li>
+            <li>${info.strIngredient6}<li>
+            <li>${info.strIngredient7}<li>
+            <li>${info.strIngredient8}<li>
+            <li>${info.strIngredient9}<li>
+            <li>${info.strIngredient10}<li>
+            <li>${info.strIngredient11}<li>
+            <li>${info.strIngredient12}<li>
+            <li>${info.strIngredient13}<li>
+            <li>${info.strIngredient14}<li>
+            <li>${info.strIngredient15}<li>
+            <li>${info.strIngredient16}<li>
         </ul>
-    `
-}
-
-const displayError = error => {
-    const errorMessage = document.getElementById('error-message');
-    errorMessage.innerText = error;
+    `;
+    recipeInfo.appendChild(recipeDiv);
+    
+    })
 }
